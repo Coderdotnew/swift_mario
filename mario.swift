28d33be1c5b7bbac6515f977f92ed55d10762537
Stephanholly <sholly@usc.edu>
2016-11-06 21:58:10 -0700

    first commit

---
+++ mario.swift
@@ -0,0 +1,38 @@
+// Mario Pyramids
+// In the game Mario, before completing each level, Mario must jump from a pyramid of blocks onto a flag pole. Your mission is to recreate these blocks using Ruby. 
+
+
+// Half pyramid
+- Write a program that creates a half pyramind. This should take in user input and build a pyramid to the height specified.
+- For example, when the program runs and the user inputs `6`, the following pyramid should return:  
+```swift
+     # 
+    ## 
+   ### 
+  #### 
+ ##### 
+###### 
+```
+
+// Full pyramid
+
+- Write program that asks a user how tall they would like to make the pyramid. This should be a non-negative integer no greater than 23 (this is the tallest that a pyramid can be in Mario land).
+- For example, when the program runs and the user inputs `3`, the following pyramid should return:  
+```swift
+     # #
+    ## ##
+   ### ###
+```
+- When the program runs and the user inputs `6`, the following pyramid should return:  
+```swift
+     # #
+    ## ##
+   ### ###
+  #### ####
+ ##### #####
+###### ######
+```
+
+##### Note: This lab is taught in the Harvard CS50 course that is taught in Java, but you'll be creating it in Swift!  
+##### GOOD LUCK!
+
